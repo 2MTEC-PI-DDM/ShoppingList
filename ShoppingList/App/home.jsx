66bdf79764f653be14bc87b50270a766a7331f0e
laislobato <laislobato210109@gmail.com>
2025-05-12 11:40:10 -0300

    0.4.0

--- ShoppingList/App/home.jsx
+++ ShoppingList/App/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, SafeAreaView, ImageBackground } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, TextInput, TouchableOpacity } from 'react-native'
 import {Ionicons} from '@expo/vector-icons'
 
 
@@ -22,8 +22,17 @@ export default function Home() {
         <View style={{ flex: 1 }}></View>
 
         <View style={styles.footer}>
-
-
+            <View style={styles.inputContainer}>
+              <TextInput 
+              color="#fff"
+              fontsize={18}
+              placeholderTextColor="#aeaeae"
+              placeholder='Digite o nome do produto....'
+              />
+            </View>
+            <TouchableOpacity style={styles.iconContainer}>
+              <Ionicons name="name" size={36} color="#fff" />
+            </TouchableOpacity>
         </View>
 
       </ImageBackground>
@@ -48,6 +57,34 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
     color:'#fff'
+  },
+  footer: {
+    backgroundColor: '#000000c0',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: '#000',
+    height: 50,
+    marginVertical: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#000',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 
 })
